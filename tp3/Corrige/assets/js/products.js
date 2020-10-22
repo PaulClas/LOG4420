cf64e5c536d99d41c9ebca279a4aa5add8537afd
m <myriamkiriakos342@gmail.com>
2020-10-21 21:59:35 -0400

    fini products.js

---
+++ tp3/Corrige/assets/js/products.js
@@ -0,0 +1,87 @@
+const $ = window.$;
+$(() =>{
+
+    const productList = $("#products-list");
+
+    $.getJSON("../../data/products.json", (json) => {
+
+        json.forEach(element => {
+            console.dir(element.name);
+            const product = `<div class="product" data-price="${element.price}" id="${element.id}" data-name="${element.name}" data-category="${element.category}">
+            <a href="./product.html?id=${element.id}" title="En savoir plus...">
+            <h2>${element.name}</h2>
+        <img alt="${element.name}" src="./assets/img/${element.image}">
+            <p class="price"><small>Prix</small> ${element.price}&thinsp;$</p>
+        </a>
+        </div>`;
+            productList.append(product);
+        });
+
+        orderByPriceLowerToHighest();
+        showCountProduct($("#products-list > .product"));
+
+    });
+});
+
+function orderByPriceLowerToHighest() {
+    $("#products-list").html($("#products-list > .product").toArray().sort((a, b) => {
+        return +a.dataset.price - +b.dataset.price; }));
+    $("#product-criteria > button").removeClass("selected");
+    $("#product-criteria > button:contains('Prix (bas-haut)')").addClass("selected");
+    console.dir($("#products-list > .product")[0]);
+
+}
+
+function orderByPriceHighestToLower() {
+    $("#products-list").html($("#products-list > .product").toArray().sort((a, b) => {
+        return +b.dataset.price - +a.dataset.price; }));
+    $("#product-criteria > button").removeClass("selected");
+    $("#product-criteria > button:contains('Prix (haut-bas)')").addClass("selected");
+}
+function orderByAlphabetically() {
+
+    $("#products-list").html($("#products-list > .product").toArray().sort((a, b) => {
+        if (b.dataset.name.toUpperCase() > a.dataset.name.toUpperCase()) { return 1; }
+        else if (b.dataset.name.toUpperCase() < a.dataset.name.toUpperCase()) { return -1; }
+        else { return 0; } }));
+
+    $("#product-criteria > button").removeClass("selected");
+    $("#product-criteria > button:contains('Nom (A-Z)')").addClass("selected");
+}
+
+function orderByAlphabeticallyInverse() {
+    console.log( $("#products-list > .product").toArray());
+    $("#products-list").html($("#products-list > .product").toArray().sort((a, b) => {
+        if (b.dataset.name.toUpperCase() < a.dataset.name.toUpperCase()) { return 1; }
+        else if (b.dataset.name.toUpperCase() > a.dataset.name.toUpperCase()) { return -1; }
+        else { return 0; } }));
+
+    $("#product-criteria > button").removeClass("selected");
+    $("#product-criteria > button:contains('Nom (Z-A)')").addClass("selected");
+}
+
+function showByCategories(category) {
+    console.dir($(`#products-list > .product[data-category='${category}']`));
+
+    $("#products-list > .product").css("display", "none");
+    $(`#products-list > .product[data-category='${category}']`).css("display", "block");
+    // $("#products-list > .product").not(`[data-category="${category}`).html($("#products-list > .product").not(`[data-category="${category}"]`).css("display", "none"));
+    // $("#products-list > .product").not(`[data-category='${category}']"`).css("display", "none");
+    // $("#products-list > .product").find(`[data-category='${category}']"`).css("display", "block");
+
+    $("#product-categories > button").removeClass("selected");
+    $(`#product-categories > button[data-category='${category}']`).addClass("selected");
+
+    showCountProduct($(`#products-list > .product[data-category='${category}']`));
+}
+
+function showAll() {
+    $("#products-list > .product").css("display", "block");
+    $("#product-categories > button").removeClass("selected");
+    $("#product-categories > button:contains('Tous les produits')").addClass("selected");
+    showCountProduct($("#products-list > .product"));
+}
+
+function showCountProduct(produits) {
+    $("#products-count").text(`${produits.length} products`);
+}
