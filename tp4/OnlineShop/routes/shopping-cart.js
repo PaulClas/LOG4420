74139dfe7ba2c832542eec6abb04d6b0b2bc675e
m <myriamkiriakos342@gmail.com>
2020-11-14 14:12:54 -0500

    certains trucs passent ds le cart

--- tp4/OnlineShop/routes/shopping-cart.js
+++ tp4/OnlineShop/routes/shopping-cart.js
@@ -6,53 +6,118 @@ const statut = require("http-status-codes");
 
 /*Panier */
 router.get("/", async (req,res)=>{
-    try{
-        console.dir(req.session);
+    try{    
+        let sess = req.session;
+        if(req.session.cart){
+            
+        res.json(req.session.cart).status(statut.StatusCodes.OK);
+        } else{
+            
+        res.json([]).status(statut.StatusCodes.OK);
+        }
 
+    
     }
     catch(err){
-        
+        console.dir("hello");
     }
 });
 
 router.get("/:productId", async(req,res)=>{
     try{
-        console.dir(req);
+        
+        console.dir(req.params);
+        
+        for(let i = 0; i < req.session.cart; i++)
+        {
+            if(req.session.cart[i].productId === req.params.productId){
+                res.json(req.session.cart[i]).status(statut.StatusCodes.OK);
+            }
+        }
+        res.json().status(statut.StatusCodes.NOT_FOUND);
     }
     catch(err){
-        
+        res.json().status(statut.StatusCodes.NOT_FOUND);   
     }
 });
 
 router.post("/", async (req,res)=>{
     try{
-        console.dir(req.body);
+        if(isNaN(req.body.quantity) || req.body.quantity < 1){
+            
+            res.status(statut.StatusCodes.BAD_REQUEST).json("quantity not ok");
+        }
+        let p = await db.findProduct(req.body.productId);
+        if(p === null){
+            res.status(statut.StatusCodes.BAD_REQUEST).json("identifiant dans liste produit nexiste pas");
+        }
+
+        if(!req.session.cart){
+            req.session.cart = new Array();
+        }
+        req.session.cart.push({productId: req.body.productId, quantity: req.body.quantity});
+        res.json().status(statut.StatusCodes.CREATED);
+        
     }
     catch(err){
         
+        res.status(statut.StatusCodes.BAD_REQUEST).json("erreur");
     }
 });
 
 router.put("/:productId", async(req,res)=>{
     try{
+        if(isNaN(req.body.quantity) || req.body.quantity < 1){
+            
+            res.status(statut.StatusCodes.BAD_REQUEST).json("quantity not ok");
+        }
+        let hasFound = false;
+        if(req.session.cart){
+            for(let i = 0; i < req.session.cart.length; i++){
+                if(req.session.cart[i].productId === req.params.productId){
+                    req.session.cart[i].quantity = req.body.quantity;
+                    hasFound = true;
+                    res.json().status(statut.StatusCodes.NO_CONTENT);
+                }
+            }
+            if(!hasFound){
+                
+                res.json().status(statut.StatusCodes.NOT_FOUND);
+            }
 
+        }
     }
     catch(err){
-        
+        res.status(statut.StatusCodes.BAD_REQUEST).json("quantity not ok");
     }
 });
 
 router.delete("/:productId", async (req,res)=>{
     try{
-
+        let hasFound = false;
+        if(req.session.cart){
+            for(let i = 0; i < req.session.cart.length; i++){
+                if(req.session.cart[i].productId === req.params.productId){
+                    req.session.cart.splice(i, 1);
+                    hasFound = true;
+                    res.json().status(statut.StatusCodes.NO_CONTENT);
+                }
+            }
+            if(!hasFound){
+                
+                res.json().status(statut.StatusCodes.NOT_FOUND);
+            }
+        }
     }
     catch(err){
         
+        res.status(statut.StatusCodes.BAD_REQUEST).json("quantity not ok");
     }
 });
 
 router.delete("/", async (req, res)=>{
-
+    req.session.cart = new Array();
+    res.json().status(statut.StatusCodes.NO_CONTENT);
 });
 
 module.exports = router;
