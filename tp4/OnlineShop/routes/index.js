7f17884cb55aace69d6c90979d7ca5cd8cd62929
m <myriamkiriakos342@gmail.com>
2020-11-19 23:39:13 -0500

    fait fonctionner produit

--- tp4/OnlineShop/routes/index.js
+++ tp4/OnlineShop/routes/index.js
@@ -3,35 +3,43 @@ const router = express.Router();
 const db = require("./../lib/db")
 
 router.get(["/", "/accueil"], (req, res) => {
-    res.render("../views/pages/index", {title: "Accueil", num:req.session.cart.length});
+    const num = req.session.cart?req.session.cart.length:0;
+    res.render("../views/pages/index", {title: "Accueil", num:num});
 });
 
 router.get("/produits", (req, res) => {
     db.findProducts(undefined, undefined).then((p)=>{
-        
-    res.render("../views/pages/products", {title: "Produits", products: p, num:req.session.cart.length});
+        const num = req.session.cart?req.session.cart.length:0;
+    res.render("../views/pages/products", {title: "Produits", products: p, num:num});
     });
 });
 
 router.get("/produits/:id", (req, res) => {
-    const product = db.findProduct(req.params.id);
-    res.render("../views/pages/product", {title: "Produit", product: product, num:req.session.cart.length});
+    db.findProduct(req.params.id).then((p) =>  {
+        const num = req.session.cart?req.session.cart.length:0;
+        res.render("../views/pages/product", {title: "Produit",product:p, num:num});
+
+    });
 });
 
 router.get("/contact", (req, res) => {
-    res.render("../views/pages/contact", {title: "Contact", num:req.session.cart.length});
+    const num = req.session.cart?req.session.cart.length:0;
+    res.render("../views/pages/contact", {title: "Contact", num:num});
 });
 
 router.get("/panier", (req, res) => {
-    res.render("../views/pages/shopping-cart", {title: "Panier", num:req.session.cart.length});
+    const num = req.session.cart?req.session.cart.length:0;
+    res.render("../views/pages/shopping-cart", {title: "Panier", num:num});
 });
 
 router.get("/commande", (req, res) => {
-    res.render("../views/pages/order", {title: "Commande", num:req.session.cart.length});
+    const num = req.session.cart?req.session.cart.length:0;
+    res.render("../views/pages/order", {title: "Commande", num:num});
 });
 
 router.get("/confirmation", (req, res) => {
-    res.render("../views/pages/confirmation", {title: "Confirmation", num:req.session.cart.length});
+    const num = req.session.cart?req.session.cart.length:0;
+    res.render("../views/pages/confirmation", {title: "Confirmation", num:num});
 });
 
 
