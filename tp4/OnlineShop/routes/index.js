9be11cd73682c628d960e86ee250bef65da5ad21
m <myriamkiriakos342@gmail.com>
2020-11-19 21:40:37 -0500

    initialisation de image fonctionne

--- tp4/OnlineShop/routes/index.js
+++ tp4/OnlineShop/routes/index.js
@@ -1,16 +1,21 @@
 const express = require("express");
 const router = express.Router();
+const db = require("./../lib/db")
 
 router.get(["/", "/accueil"], (req, res) => {
     res.render("../views/pages/index", {title: "Accueil", cart: req.session.cart});
 });
 
 router.get("/produits", (req, res) => {
-    res.render("../views/pages/products", {title: "Produits", produits: req.session.cart});
+    db.findProducts(undefined, undefined).then((p)=>{
+        
+    res.render("../views/pages/products", {title: "Produits", products: p});
+    });
 });
 
 router.get("/produits/:id", (req, res) => {
-    res.render("../views/pages/product", {title: "Produit"});
+    const product = db.findProduct(req.params.id);
+    res.render("../views/pages/product", {title: "Produit", product: product});
 });
 
 router.get("/contact", (req, res) => {
