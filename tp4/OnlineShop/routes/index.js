9746499537486cf56109a824ae31faeb49af0bb0
m <myriamkiriakos342@gmail.com>
2020-11-19 21:50:55 -0500

    arranger le num

--- tp4/OnlineShop/routes/index.js
+++ tp4/OnlineShop/routes/index.js
@@ -3,35 +3,35 @@ const router = express.Router();
 const db = require("./../lib/db")
 
 router.get(["/", "/accueil"], (req, res) => {
-    res.render("../views/pages/index", {title: "Accueil", cart: req.session.cart});
+    res.render("../views/pages/index", {title: "Accueil", num:req.session.cart.length});
 });
 
 router.get("/produits", (req, res) => {
     db.findProducts(undefined, undefined).then((p)=>{
         
-    res.render("../views/pages/products", {title: "Produits", products: p});
+    res.render("../views/pages/products", {title: "Produits", products: p, num:req.session.cart.length});
     });
 });
 
 router.get("/produits/:id", (req, res) => {
     const product = db.findProduct(req.params.id);
-    res.render("../views/pages/product", {title: "Produit", product: product});
+    res.render("../views/pages/product", {title: "Produit", product: product, num:req.session.cart.length});
 });
 
 router.get("/contact", (req, res) => {
-    res.render("../views/pages/contact", {title: "Contact"});
+    res.render("../views/pages/contact", {title: "Contact", num:req.session.cart.length});
 });
 
 router.get("/panier", (req, res) => {
-    res.render("../views/pages/shopping-cart", {title: "Panier"});
+    res.render("../views/pages/shopping-cart", {title: "Panier", num:req.session.cart.length});
 });
 
 router.get("/commande", (req, res) => {
-    res.render("../views/pages/order", {title: "Commande"});
+    res.render("../views/pages/order", {title: "Commande", num:req.session.cart.length});
 });
 
 router.get("/confirmation", (req, res) => {
-    res.render("../views/pages/confirmation", {title: "Confirmation"});
+    res.render("../views/pages/confirmation", {title: "Confirmation", num:req.session.cart.length});
 });
 
 
