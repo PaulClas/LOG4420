bb4ea7bd64ec02b924c3996b6518f3cfd8eb0eeb
Paul Clas <clas.paul@gmail.com>
2020-11-28 20:05:35 -0500

    footer done

--- tp5/CodeInitial/client/src/_Common/Footer.js
+++ tp5/CodeInitial/client/src/_Common/Footer.js
@@ -2,7 +2,7 @@
 export function Footer() {
   return (
     <footer>
-        <p>Équipe 210 - Myriam Kiriakos 1888929 et Paul Clas 1846912</p>
+        <p>Équipe 210 - Myriam Kiriakos 1888929 et Paul Clas 1846912git</p>
     </footer>
   );
 }
